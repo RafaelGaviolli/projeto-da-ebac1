a3f4d23ff95db2176dc2dc79dff2c06f256ff15b
Rafa <bygamerrcg@hotmail.com>
2022-01-10 23:40:08 -0300

    primeiro commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String ambiente = args[0];
+        if (ambiente.equalsIgnoreCase("DEV")) {
+            System.out.print("Executando em DEV");
+        } else if (ambiente.equalsIgnoreCase("TEST")) {
+            System.out.print("Executando test");
+        } else {
+            System.out.print(ambiente);
+        }
+    }
+}
